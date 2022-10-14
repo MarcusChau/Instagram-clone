a488e00a249026b290fb45f904127b72f281d9af
Marcus Chau <marcusc8786@gmail.com>
2022-10-14 15:13:33 -0400

    Created the posts, and utilized React Hook and also state to pass props and display them

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,33 @@
-import React from 'react' /* rfce command  */
+import React from 'react'; /* rfce command  */
+import './Post.css';
 
-function Post() {
+
+// Post function 
+// destructuring the props - ES6 syntax
+function Post({username, caption, imageUrl}) {
   return (
-    <div>
+    <div className='post'>
+
         {/* header -> avatar + username */}
+        <div className="post__header">
+            {/* Avatar */}
+            <img 
+            className='post__avatar' 
+            src="https://media.istockphoto.com/photos/aerial-view-of-lower-manhattan-new-york-picture-id946087016?k=20&m=946087016&s=612x612&w=0&h=5k1qMeooPXIBjCt6R5nUi_Mb4_PkhliKpBXDnfEGGww=" 
+            alt="Avatar" 
+            />
+
+            {/* Username */}
+            <h3>{username}</h3>
+        </div>
+
 
         {/* Image */}
+        <img className='post__image' src={imageUrl} alt="Holder"/>
+
 
         {/* username + caption */}
+        <h4 className='post__text'><strong>{username}</strong> {caption}</h4>
     </div>
   )
 }
